4019a93e58050d0ad5d64c82c24ca8468a8744dc
hwalborn <hwalborn@gmail.com>
2017-11-06 22:04:05 -0500

    removed debugger

--- src/login.js
+++ src/login.js
@@ -9,7 +9,6 @@ login(){
 
   firebase.auth().signInWithPopup(provider).then(function(result) {
     // This gives you a Google Access Token. You can use it to access the Google API.
-    debugger;
     var token = result.credential.accessToken;
     // The signed-in user info.
     var user = result.user;
