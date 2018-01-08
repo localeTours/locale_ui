af4049464c5d31c949250000cdf1c0fab4940c03
Aaron Figueroa <aaronfigueroa@Aarons-MacBook-Pro.local>
2018-01-07 20:35:08 -0600

    rounded card corners. took away hamburger menu if user is able to see icons

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -12,13 +12,17 @@ import Dashboard from '../../views/Dashboard/';
 import Charts from '../../views/Charts/';
 import Widgets from '../../views/Widgets/';
 import Profile from '../../frontend/views/Profile';
+import Explore from '../../frontend/components/explore';
 import firebase from '../../fire';
 
 
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 import { bindActionCreators } from 'redux';
-import { signIn } from '../../actions';
+import { signIn } from '../../frontend/actions';
+
+
+//Components
 
 
 // Components
@@ -74,18 +78,7 @@ class Full extends Component {
                                 <Switch>
                                     <Route path="/user/:id" name="Profile" component={Profile}/>
                                     <Route path="/dashboard" name="Dashboard" component={Dashboard}/>
-                                    <Route path="/components/buttons" name="Buttons" component={Buttons}/>
-                                    <Route path="/components/cards" name="Cards" component={Cards}/>
-                                    <Route path="/components/forms" name="Forms" component={Forms}/>
-                                    <Route path="/components/modals" name="Modals" component={Modals}/>
-                                    <Route path="/components/social-buttons" name="Social Buttons" component={SocialButtons}/>
-                                    <Route path="/components/switches" name="Swithces" component={Switches}/>
-                                    <Route path="/components/tables" name="Tables" component={Tables}/>
-                                    <Route path="/components/tabs" name="Tabs" component={Tabs}/>
-                                    <Route path="/icons/font-awesome" name="Font Awesome" component={FontAwesome}/>
-                                    <Route path="/icons/simple-line-icons" name="Simple Line Icons" component={SimpleLineIcons}/>
-                                    <Route path="/widgets" name="Widgets" component={Widgets}/>
-                                    <Route path="/charts" name="Charts" component={Charts}/>
+                                    <Route path="/explore" name="explore" component={Explore}/>
                                     <Redirect from="/" to="/dashboard"/>
                                 </Switch>
                             </Container>
