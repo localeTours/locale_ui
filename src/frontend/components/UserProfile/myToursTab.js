3472d81135d9827ea1032079f1519f7c3bd8c2fa
Aaron Figueroa <aaronfigueroa@Aarons-MacBook-Pro.local>
2018-01-24 07:27:18 -0600

    LOCALE-22: User tours are now shown in the table of the my Profile page. Set up sent to me component as well

--- src/frontend/components/UserProfile/myToursTab.js
+++ src/frontend/components/UserProfile/myToursTab.js
@@ -1,48 +1,40 @@
 import React, {Component} from 'react';
-import {Row, Col, Card, CardHeader, CardBody, Button, Badge, TabContent, TabPane, Nav, NavItem, NavLink, Table, Progress} from 'reactstrap';
-import classnames from 'classnames';
 
 
 export default class MyToursTab extends Component {
     render(){
 
         return(
-
-            <tbody>
-                <tr>
-                    <td className="text-center">
-                        <div className="avatar">
-                            <img src={'img/avatars/1.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
-                            <span className="avatar-status badge-success"></span>
-                        </div>
-                    </td>
-                    <td>
-                        <div>Tour Name - {this.props.info.tour}</div>
-                    </td>
-                    <td className="text-center">
-                        <img src={'img/flags/USA.png'} alt="USA" style={{height: 24 + 'px'}}/>
-                    </td>
-                    <td>
-                        <div className="clearfix">
-                            <div className="float-left">
-                                <strong>New York City</strong>
+                <tbody>
+                    <tr>
+                        <td className="text-center">
+                            <div className="avatar">
+                                <img src={'img/avatars/1.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com"/>
+                                <span className="avatar-status badge-success"></span>
                             </div>
-                            <br/>
-                            <div className="float-left">
-                                <small className="text-muted">Mid-town Manhattan</small>
+                        </td>
+                        <td>
+                            <div className="clearfix">
+                                <div className="float-left">
+                                    <strong>Tour Name </strong>
+                                </div>
+                                <br/>
+                                <div className="float-left">
+                                    <small className="text-muted">{this.props.info.tour}</small>
+                                </div>
                             </div>
-                        </div>
-                    </td>
-                    <td className="text-center">
-                        <i className="fa fa-bicycle" style={{fontSize: 24 + 'px'}}></i>
-                    </td>
-                    <td>
-                        <div className="small text-muted">July</div>
-                        <strong>10 sec ago</strong>
-                    </td>
-                </tr>
+                        </td>
 
-            </tbody>
+                        <td className="text-center">
+                            <i className="fa fa-bicycle" style={{fontSize: 24 + 'px'}}></i>
+                        </td>
+                        <td>
+                            <div className="small text-muted">July</div>
+                            <strong>10 sec ago</strong>
+                        </td>
+                    </tr>
+
+                </tbody>
         )
     }
 
