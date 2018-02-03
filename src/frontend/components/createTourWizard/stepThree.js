bab68b9337434005b56ee7826c2ef6ae250fd786
Ivan Eduardo Felix Herrera <ivanfex@Ivans-MBP.home>
2018-02-03 00:14:12 -0500

    added map to second step

--- src/frontend/components/createTourWizard/stepThree.js
+++ src/frontend/components/createTourWizard/stepThree.js
@@ -16,5 +16,3 @@ export default class StepThree extends React.Component {
 
 
 }
-
-
--- src/frontend/components/createTourWizard/stepTwo.js
+++ src/frontend/components/createTourWizard/stepTwo.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Form, FormGroup, Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 // import MapContainer from '../MapContainer';
 
+import './index.css';
+
 export default class StepTwo extends React.Component {
   constructor(){
       super();
       this.state = {
           checkpoints: [],
-          modal: false
+          modal: false,
+          zoom: 13,
+          maptype: 'roadmap',
+          place_formatted: '',
+          place_id: '',
+          place_location: ''
       }
       // this.tours = [];
       this.makeCheckpoint = this.makeCheckpoint.bind(this);
@@ -49,6 +56,55 @@ export default class StepTwo extends React.Component {
      console.log(checkIdArr);
  }
 
+ componentDidMount() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap',
+    });
+
+    map.addListener('zoom_changed', () => {
+      this.setState({
+        zoom: map.getZoom(),
+      });
+    });
+
+    map.addListener('maptypeid_changed', () => {
+      this.setState({
+        maptype: map.getMapTypeId(),
+      });
+    });
+
+    let marker = new window.google.maps.Marker({
+      map: map,
+      position: {lat: -33.8688, lng: 151.2195},
+    });
+
+    // initialize the autocomplete functionality using the #pac-input input box
+    let inputNode = document.getElementById('pac-input');
+    map.controls[window.google.maps.ControlPosition.TOP_LEFT].push(inputNode);
+    let autoComplete = new window.google.maps.places.Autocomplete(inputNode);
+
+    autoComplete.addListener('place_changed', () => {
+      let place = autoComplete.getPlace();
+      let location = place.geometry.location;
+
+      this.setState({
+        place_formatted: place.formatted_address,
+        place_id: place.place_id,
+        place_location: location.toString(),
+      });
+
+      // bring the selected place in view on the map
+      map.fitBounds(place.geometry.viewport);
+      map.setCenter(location);
+
+      marker.setPlace({
+        placeId: place.place_id,
+        location: location,
+      });
+    });
+  }
 
    makeCheckpoint(e){
     //Makes the checkpoint then adds to checkpoint state
@@ -69,6 +125,9 @@ export default class StepTwo extends React.Component {
 
   render(){
     // console.log(this.props)
+    let style = {
+     height: '300px'
+    }
     let tourId = this.props.tourId
     return (
      <div>
@@ -83,6 +142,10 @@ export default class StepTwo extends React.Component {
             <ModalHeader toggle={this.toggle}>MAP</ModalHeader>
             <ModalBody>
               <div className='modal-main'>
+                  <div id='pac-container'>
+                    <input id='pac-input' type='text' placeholder='Enter a location' />
+                  </div>
+                  <div id='map' style={style}/>
                 <div className='modal-inputs'>
                   <Input type='text' placeholder='title/message'/>
                   <Input type='text' placeholder='Add destination or click on the map'/>
@@ -103,6 +166,7 @@ export default class StepTwo extends React.Component {
           <input type="number" ref="lat" placeholder='latitude'/>
           <input type="number" ref="long" placeholder='longitude' /> */}
           <Form>
+
            <FormGroup>
              <Label for="checkpoints">Checkpoint(s):</Label>
              <Input type="text" ref="checkpointName" placeholder='name'/>
@@ -127,7 +191,10 @@ export default class StepTwo extends React.Component {
 
         </div>
         <div className='step-two-col-2'>
-          MAP
+          <div id='pac-container'>
+            <input id='pac-input' type='text' placeholder='Enter a location' />
+          </div>
+          <div id='map' />
         </div>
         {/* <p>{tourId}</p> */}
       </div>
